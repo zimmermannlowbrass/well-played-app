c19fe04080044403f6754fb783af1696138f4993
James Aidan Zimmermann <zimmermann.lowbrass@gmail.com>
2023-06-28 13:37:07 -0400

    first app routes

--- server/app.py
+++ server/app.py
@@ -3,7 +3,7 @@
 # Standard library imports
 
 # Remote library imports
-from flask import request
+from flask import request, make_response, jsonify
 from flask_restful import Resource
 
 # Local imports
@@ -11,6 +11,57 @@ from config import app, db, api
 from models import User, Playground, CheckIn
 
 # Views go here!
+
+@app.route('/')
+def index():
+    return "Welcome to the WellPlayed App!"
+
+@app.route('/playgrounds')
+def playgrounds():
+    playgrounds = []
+    for playground in Playground.query.all():
+        pg_dict = {
+            "name": playground.name,
+            "neighborhood": playground.neighborhood,
+            "has_restroom": playground.has_restroom,
+            "has_water_feature": playground.has_water_feature
+        }
+        playgrounds.append(pg_dict)
+    response = make_response(
+        jsonify(playgrounds),
+        200
+    )
+    return response
+
+
+@app.route('/users')
+def users():
+    users = []
+    for user in User.query.all():
+        user_dict = {
+            "name": user.name,
+            "rank": user.rank
+        }
+        users.append(user_dict)
+    response = make_response(
+        jsonify(users),
+        200
+    )
+    return response
+    
+@app.route('/users/<int:id>')
+def user_by_id(id):
+    user = User.query.filter(User.id == id).first()
+    user_dict = {
+            "name": user.name,
+            "rank": user.rank
+        }
+    response = make_response(
+        jsonify(user_dict),
+        200
+    )
+    return response
+    
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -5,8 +5,10 @@ from config import db
 
 # Models go here!
 
-class User(db.Model):
+class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
+
+    serialize_rules = ('-checkins.user')
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
@@ -17,8 +19,10 @@ class User(db.Model):
     def __repr__(self):
         return f'<User: {self.name}>'
 
-class Playground(db.Model):
+class Playground(db.Model, SerializerMixin):
     __tablename__ = 'playgrounds'
+
+    serialize_rules = ('-checkins.playground')
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
@@ -31,8 +35,10 @@ class Playground(db.Model):
     def __repr__(self):
         return f'<Playground: {self.name}>'
 
-class CheckIn(db.Model):
+class CheckIn(db.Model, SerializerMixin):
     __tablename__ = 'checkins'
+
+    serialize_rules = ('-user.checkins', '-playground.checkins')
 
     id = db.Column(db.Integer, primary_key=True)
     rating = db.Column(db.Integer)
