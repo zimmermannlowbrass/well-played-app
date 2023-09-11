c584fefe76e826175efd30b7d0abaec9fde6b46c
James Aidan Zimmermann <zimmermann.lowbrass@gmail.com>
2023-09-11 15:11:01 -0400

    add a scraper to grab every playground name

--- server/scraper.py
+++ server/scraper.py
@@ -20,3 +20,24 @@ def scraper(playground):
 
 
 print(scraper("abc-playground"))
+
+
+def scraper2():
+    site = f"https://www.nycgovparks.org/facilities/playgrounds?boro=manhattan"
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"}
+    req = Request(site,headers=headers)
+    page = urlopen(req).read()
+
+    doc = BeautifulSoup(page, "html.parser")
+
+    playground_table = doc.select('table')[0]
+
+    playground_names = playground_table.select('strong')
+    for playground_name in playground_names:
+        if playground_name.select('a'):
+            print(playground_name.select('a')[0].contents)
+        else:
+            print(playground_name.contents)
+    
+
+scraper2()
